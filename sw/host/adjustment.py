fe64aa41e724527a919f7b327a803fd2cddb12bd
Денис Сирица <dsiritsa@nppntt.ru>
2019-12-16 17:52:34 +0300

    calibration process

--- sw/host/adjustment.py
+++ sw/host/adjustment.py
@@ -5,9 +5,11 @@ from PIL import Image, ImageDraw
 import numpy as np
 
 line_scan_bytes_size = 2592 * 6
-fname_dark  = "f:/testfile_dark.pcm"
-fname_white = "f:/testfile_white_0.pcm"
+#fname_dark  = "f:/testfile_dark.pcm"
+#fname_white = "f:/testfile_white_0.pcm"
 
+fname_dark  = "/home/denc/Dropbox/Upload/adjust/testfile_dark.pcm"
+fname_white = "/home/denc/Dropbox/Upload/adjust/testfile_white_0.pcm"
 
 
 
@@ -20,6 +22,7 @@ def save_colors_file(rgb, type = 1, fnames = ["out_red.pcm", "out_green.pcm", "o
         fds[i] = open(fnames[i], "wb")
 
     lines_cnt = rgb.shape[0]
+    #print("lines_cnt=%d" % lines_cnt)
 
     for line_idx in range(lines_cnt):
         line = np.transpose(rgb[line_idx][:])
@@ -68,22 +71,79 @@ def load_pcm(fname):
 
 #calc mean value of every color on all lines
 def calc_average_rgb(rgb):
-    lines_cnt = rgb.shape[0]
     rgb_aver = np.int16(np.average(rgb, axis = 0))
     return rgb_aver    
 
 dark = load_pcm(fname_dark)
 white = load_pcm(fname_white)
 
+##################################################
+##correction
+#white[:,:,1] = np.multiply(white[:,:,1], 0.81)
+#white[:,:,2] = np.multiply(white[:,:,2], 0.9)
+##################################################
+
 dark_mean = calc_average_rgb(dark)
 dark_mean = np.int16(np.average(dark_mean, axis = 1))
-print("dark_mean: %d, %d" %(np.max(dark_mean), np.min(dark_mean)))
+print("dark_mean: max=%d, min=%d" %(np.max(dark_mean), np.min(dark_mean)))
 
 
 white_mean = calc_average_rgb(white)
-print("white_mean: %d, %d" %(np.max(white_mean), np.min(white_mean)))
+print("white_mean: max=%d, min=%d" %(np.max(white_mean), np.min(white_mean)))
+save_colors_file(np.reshape(white_mean, (1, 2592,3)), 1, ["out_red_white.pcm", "out_green_white.pcm", "out_blue_white.pcm"])
 
-print(dark_mean.shape)
-print(white_mean.shape)
+#print(dark_mean.shape)
+#print(white_mean.shape)
+
+Vdmin = np.min(dark_mean)
+print("Vdmin = %d" % Vdmin)
+
+Vp  = np.zeros_like(white_mean)
+VEp = np.zeros_like(white_mean)
+for i in range(3):
+    Vp[:,i]  = white_mean[:,i] - Vdmin
+    VEp[:,i] = white_mean[:,i] - dark_mean[:]
+
+
+
+#step3 find MIN[Vpmax] across all colors
+Vpmax = np.zeros(3)
+for i in range(3):
+    Vpmax[i] = np.max(Vp[:,i])
+print("Vpmax'es: " + str(Vpmax))
+Vpmax_min = np.min(Vpmax)
+print("MIN[Vpmax] " + str(Vpmax_min))
+
+#step4 here should be correction MIN[Vpmax] for Vpmax from table
+
+
+
+#step5 calculate Vp(n) on color RED for example and store to Vp_avg_r
+Vp_avg_r = np.average(Vp[:,0])
+print("Vp_avg_r: " + str(Vp_avg_r))
+Vp_avg_min = Vp_avg_r - Vp_avg_r*0.05
+Vp_avg_max = Vp_avg_r + Vp_avg_r*0.05
+print("AVG[Vp] 5 percents boundaries: %f, %f" % (Vp_avg_min, Vp_avg_max))
+
+#step6 calc and tune other two AVG[Vp]
+Vp_avg_g = np.average(Vp[:,1])
+Vp_avg_b = np.average(Vp[:,2])
+
+print("Vp_avg_g: " + str(Vp_avg_g))
+print("Vp_avg_b: " + str(Vp_avg_b))
+
+if(Vp_avg_g < Vp_avg_min or Vp_avg_g > Vp_avg_max):
+    print("Vp_avg_g is not in boundaries!")
+else:
+    print("Vp_avg_g ok!")
+
+if(Vp_avg_b < Vp_avg_min or Vp_avg_g > Vp_avg_max):
+    print("Vp_avg_b is not in boundaries!")
+else:
+    print("Vp_avg_b ok!")
+
+
+Вычислить UEp и увидеть что она меньше 50%
+Вычислить Ud и проверить на условие из таблицы
 
 input("done, press Enter...")
--- sw/host/parser_RGB_bin.py
+++ sw/host/parser_RGB_bin.py
@@ -5,15 +5,13 @@ from PIL import Image, ImageDraw
 import numpy as np
 
 line_scan_bytes_size = 2592 * 6
-fname = "f:/testfile.pcm"
+#fname = "f:/testfile.pcm"
 #fname = "f:/testfile_leds_off.pcm"
 #fname = "f:/testfile_white.pcm"
 #fname = "d:/Dropbox/Upload/black/testfile_black_leds_off.pcm"
 
 
-#fname = "/home/denc/Dropbox/Upload/black/testfile_black_leds_off.pcm"
-#fname = "/home/denc/Dropbox/Upload/black/testfile_01.pcm"
-#fname = "/home/denc/Dropbox/Upload/black/testfile_00.pcm"
+fname = "/home/denc/Dropbox/Upload/adjust/testfile_white_0.pcm"
 
 
 
@@ -67,7 +65,6 @@ def load_colors_from_file(fnames = ["out_red_aver_.pcm", "out_green_aver_.pcm", 
 
 
 
-
 def calc_average(rgb):
     lines_cnt = rgb.shape[0]
     #rgb_aver = np.int16(np.average(rgb[0:lines_cnt,:,:], axis = 0))
@@ -83,50 +80,63 @@ def calc_average(rgb):
 
 
 
+def load_pcm(fname):
+    f = open(fname, "rb")
+    fsize   = os.path.getsize(fname)
+    if(fsize % line_scan_bytes_size):
+        print("*****error file size! not integer linescan bytes size!!!!!")
+        sys.exit()
+
+    raw_data = np.zeros((fsize >> 1,1), dtype = np.int16)
+
+    word_color = 1
+    words_cnt = 0
+    while word_color:
+        word_color = f.read(2)
+        if word_color:
+            raw_data[words_cnt] = int.from_bytes(word_color, "little")
+            words_cnt = words_cnt + 1
+
+    lines_cnt = int((words_cnt*2) / line_scan_bytes_size)
+
+    #ignoring first RGB line
+    raw_data = raw_data[2592*3:]
+    lines_cnt = lines_cnt - 1   
+    rgb = np.reshape(raw_data,(lines_cnt,2592,3))
+
+    print("file info: %s\t lines count = %d \t bytes cnt = %d Bytes" % (fname,lines_cnt,words_cnt*2))     
+    print("max,min = %d, %d" % (np.max(raw_data), np.min(raw_data)))
+
+    f.close()
+
+    return rgb
 
 
-f = open(fname, "rb")
-
-fsize   = os.path.getsize(fname)
-if(fsize % line_scan_bytes_size):
-    print("*****error file size! not integer linescan bytes size!!!!!")
-    sys.exit()
-
-raw_data = np.zeros((fsize >> 1,1), dtype = np.int16)
-#raw_data = np.zeros((fsize >> 1,1))
-
-word_color = 1
-words_cnt = 0
-while word_color:
-    word_color = f.read(2)
-    if word_color:
-        raw_data[words_cnt] = int.from_bytes(word_color, "little")
-        words_cnt = words_cnt + 1
-        #raw_data = np.append(raw_data, int.from_bytes(word_color, "little"))
-
-lines_cnt = int((words_cnt*2) / line_scan_bytes_size)
-
-print("file info: %s\t lines count = %d \t bytes cnt = %d Bytes" % (fname,lines_cnt,words_cnt*2))     
-print("max,min = %d, %d" % (np.max(raw_data), np.min(raw_data)))
-
-#ignoring first RGB line
-raw_data = raw_data[2592*3:]
-lines_cnt = lines_cnt - 1   
 
 
 
-rgb = np.reshape(raw_data,(lines_cnt,2592,3))
+
+rgb = load_pcm(fname)
+lines_cnt = rgb.shape[0]
 save_colors_file(rgb, 1)
+
+##################################################
+##correction
+#rgb[:,:,1] = np.multiply(rgb[:,:,1], 0.81)
+#rgb[:,:,2] = np.multiply(rgb[:,:,2], 0.9)
+##################################################
 
 
 calc_store_mean = 0
 ###calculate 3 RGB averages on black lines
+'''
 if calc_store_mean == 1:
     rgb_aver = calc_average(rgb)
     save_colors_file(rgb_aver, 1, ["out_red_aver.pcm", "out_green_aver.pcm", "out_blue_aver.pcm"])
 else:
     ###if correction samples already calculated and stored
     rgb_aver = load_colors_from_file(["out_red_aver.pcm", "out_green_aver.pcm", "out_blue_aver.pcm"])
+'''
 
 ###correcting
 #for idx in range(3):
